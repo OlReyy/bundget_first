4330a6a0e8f0b19969740e4dc1576c8218ebe847
OlReyy <uvarovaolya2005@gmail.com>
2022-07-30 17:26:17 +0300

    Updates

---
+++ precache-manifest.e76ff78a4534838541fe426e8eee7977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ea6999651057d622c3e",
+    "url": "/bundget_first/static/js/main.6ea69996.chunk.js"
+  },
+  {
+    "revision": "69ab9559777502fa57ed",
+    "url": "/bundget_first/static/css/1.7a4035c7.chunk.css"
+  },
+  {
+    "revision": "69ab9559777502fa57ed",
+    "url": "/bundget_first/static/js/1.69ab9559.chunk.js"
+  },
+  {
+    "revision": "4e386f06dd9f42d2f9aa",
+    "url": "/bundget_first/static/js/runtime~main.4e386f06.js"
+  },
+  {
+    "revision": "b5fd4f76a50d49b18f3dc1a6a64741ad",
+    "url": "/bundget_first/index.html"
+  }
+];
